2f39b6a12033a79e2c356559c998fea4507dd25e
jcce <toshajo99@gmail.com>
2019-04-19 00:32:04 -0500

    compartmentalized pages, nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
+  get "shop", to: "pages#shop"
+  get "gift_ideas", to: "pages#gift_ideas"
+  get "about", to: "pages#about"
+  get "our_team", to: "pages#our_team"
+  get "news", to: "pages#news"
+  get "contact", to: "pages#contact"
+
+
+
 end
